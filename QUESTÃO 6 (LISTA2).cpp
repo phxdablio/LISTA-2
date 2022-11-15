6ae8dccddc019ce6f87f7fc02bac267db380455c
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2022-11-15 11:40:37 -0300

    Add files via upload

---
+++ QUESTÃO 6 (LISTA2).cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+int a,b,x;
+
+printf ("A= 12 B= 13\n");
+
+a=12;
+b=15;
+
+printf (" O RESULTADO É %d\n",x = a + b );
+
+
+}
+
