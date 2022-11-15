4124c2df44129f4128ec829ce1427f83f8e564d9
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2022-11-15 11:17:08 -0300

    Add files via upload

---
+++ QUESTÃO 4 (LISTA2).cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+int a,b,x;
+printf ("DE UM VALOR PARA A E PARA B\n");
+scanf ("%d %d",&a,&b);
+
+x = a + b;
+
+printf ("RESULTADO DE A + B E %d\n",x);
+
+
+}
+
