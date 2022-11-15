eaa0aa092e04459d673ebfa446ec5d5da249309c
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2022-11-15 11:48:45 -0300

    Add files via upload

---
+++ QUESTÃO 8 (LISTA2).cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+int a,b,x;
+
+printf ("DE UMA VALOR PARA A E OUTRO PARA B");
+scanf ("%d %d",&a,&b);
+
+x = a*b;
+
+printf ("0 VALOR DO PRODUTO E %d",x);
+}
